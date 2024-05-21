22d3ad703c4d4c707b45aeb167fe1379280f30c6
YUNHWAN <629jyh7@naver.com>
2024-05-21 16:54:18 +0900

    [react] 페이징 처리(화면 페이징 구현 및 API 연동)

--- blog/src/routes/BoardList.js
+++ blog/src/routes/BoardList.js
@@ -9,53 +9,83 @@ import Nav from 'react-bootstrap/Nav';
 const BoardList = () => {
 
     const [boardList, setBoardList] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [pagination, setPagination] = useState([]);
 
-    const getBoardList = async () => {
-        const resp = (await axios.get('//localhost:8080/api/posts/all')).data
-        setBoardList(resp.data)
+    const getBoardList = async (page) => {
+        const resp = (await axios.get(`//localhost:8080/api/posts/all?page=${page}`)).data
+        setBoardList(resp.data.list)
+        setPagination(resp.data.pagination)
     }
 
     useEffect(()=>{
-        getBoardList();
-    }, []);
+        getBoardList(currentPage);
+    }, [currentPage]);
 
-    let active = 2;
-    let items = [];
-    for (let number = 1; number <= 5; number++) {
-    items.push(
-            <Pagination.Item key={number} active={number === active}>
-            {number}
-            </Pagination.Item>,
+    const handlePageChange = (newPage) => {
+        if (newPage < 1 || newPage > pagination.totalPages) return;
+        setCurrentPage(newPage);
+    };
+
+    const Pagination = ({ totalPageCount, startPage, endPage, existPrevPage, existNextPage, onPageChange }) => {
+        const pages = [];
+        for (let i = startPage; i <= endPage; i++) {
+            pages.push(i);
+        }
+    
+        return (
+
+            <nav aria-label="Page navigation example" style={{marginLeft:"35%"}}>
+                <ul className="pagination">
+                    <li className="page-item">
+                    <button className="page-link"  onClick={() => onPageChange(startPage - 1)}
+                     disabled={!existPrevPage} aria-label="Prev">
+                        <span aria-hidden="true">&laquo;</span>
+                    </button>
+                    </li>
+                    {pages.map(page => (
+                        <li className={`page-item ${page === currentPage ? 'active' : ''}`} key={page}>
+                            <button className="page-link"
+                                onClick={() => onPageChange(page)}
+                                disabled={page === currentPage}>
+                                {page}
+                            </button>
+                        </li>
+                    ))}
+                    <li className="page-item">
+                    <button className="page-link"  onClick={() => onPageChange(endPage + 1)}
+                     disabled={!existNextPage} aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </button>
+                    </li>
+                </ul>
+
+                
+            </nav>
         );
-    }
+    };
 
     return(
         <div style={{marginTop:"2%", marginLeft:"20%", width:"60%"}}>
-             <Button style={{margin:"1%", marginLeft:"91%"}} variant="secondary">
-                <Nav.Link href={`/board/create`}>글쓰기</Nav.Link>
-            </Button>{' '}
-        
         {/* <Table striped bordered hover variant="light"> */}
-        <table class = "table table-hover">
-            <thead class="table-dark">
+        <table className = "table table-hover">
+            <thead className ="table-dark">
                 <tr>
-                <th>번호</th>
-                <th>제목</th>
-                <th>작성자</th>
-                <th>작성일</th>
-                <th>카테고리</th>
+                    <th style={{width:"10%"}}>번호</th>
+                    <th style={{width:"10%"}}>제목</th>
+                    <th style={{width:"10%"}}>작성자</th>
+                    <th style={{width:"10%"}}>작성일</th>
+                    <th style={{width:"10%"}}>카테고리</th>
                 </tr>
             </thead>
             <tbody>
                
                 {boardList.map((board) => (
-
                         <tr key={board.id}>
                             <td>{board.id}</td>
-                            
                             <td><Link to={`/board/${board.id}`}>{board.title}</Link></td>
                             <td>{board.writer}</td>
-                            <td>{board.updateDate}</td>
+                            <td>{new Date(board.updateDate).toLocaleString()}</td>
                             <td>{board.category}</td>
                         </tr>
                     
@@ -65,7 +95,18 @@ const BoardList = () => {
         {/* </Table> */}
         </table>
 
-        <Pagination style={{marginLeft:"0%"}}>{items}</Pagination>
+        <Button style={{margin:"1%", marginLeft:"90%"}} variant="secondary">
+                <Nav.Link href={`/board/create`}>글쓰기</Nav.Link>
+            </Button>{' '}
+
+        <Pagination
+                totalPageCount={pagination.totalPageCount}
+                startPage={pagination.startPage}
+                endPage={pagination.endPage}
+                existPrevPage={pagination.existPrevPage}
+                existNextPage={pagination.existNextPage}
+                onPageChange={handlePageChange}
+        />
         <br />
         </div>
     );
